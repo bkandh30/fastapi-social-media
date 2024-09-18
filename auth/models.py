f62904cedc57b3b3a0c464520c5f248be113cc68
bkandh30 <kandhari.bhavya20@gmail.com>
2024-09-17 21:11:15 -0700

    added user model

---
+++ auth/models.py
@@ -0,0 +1,26 @@
+#Data Models
+from sqlalchemy import Column, Date, DateTime, Integer, String, Enum, ForeignKey
+from datetime import datetime, timezone
+from sqlalchemy.orm import relationship
+
+from database import Base
+from enums import Gender
+
+#User Class
+class User(Base):
+    __tablename__ = "users"
+
+    #Basic Login details
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    name = Column(String)
+    hashed_password = Column(String, nullable=False)
+    created_date = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+
+    #Profile Details
+    dob = Column(Date)
+    gender = Column(Enum(Gender))
+    profile_pic = Column(String) #Link of user display picture
+    bio = Column(String)
+    location = Column(String)
