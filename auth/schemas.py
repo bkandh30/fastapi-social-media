f46c4c55a7cc5b17ac8a3002003234396d057408
bkandh30 <kandhari.bhavya20@gmail.com>
2024-09-17 21:23:50 -0700

    added database schema

---
+++ auth/schemas.py
@@ -0,0 +1,38 @@
+#Database Schema
+from pydantic import BaseModel
+from datetime import date, datetime
+from typing import Optional
+
+from enums import Gender
+
+#Data provided by user
+class UserBase(BaseModel):
+    email: str
+    username: str
+    name: str
+    dob: Optional[date] = None
+    gender: Optional[Gender] = None
+    bio: Optional[Gender] = None
+    location: Optional[str] = None
+    profile_pic: Optional[str] = None
+
+#Data created by user
+class UserCreate(UserBase):
+    password: str
+
+#Data Provided by user that can be updated
+class UserUpdate(BaseModel):
+    name: Optional[str] = None
+    dob: Optional[date] = None
+    gender: Optional[Gender] = None
+    bio: Optional[Gender] = None
+    location: Optional[str] = None
+    profile_pic: Optional[str] = None
+
+#Database object can create this pydantic model
+class User(UserBase, UserUpdate):
+    id: int
+    created_date: datetime
+
+    class Config:
+        orm_mode = True
