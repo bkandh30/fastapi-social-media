5cde825535902fa61a6e8c8b97e334e0bb3c91ce
bkandh30 <kandhari.bhavya20@gmail.com>
2024-09-25 14:56:23 -0700

    added follower class

--- auth/models.py
+++ auth/models.py
@@ -8,6 +8,21 @@ from .enums import Gender
 
 from post.models import post_likes, Post
 
+#Follower Class
+class Follow(Base):
+    __tablename__ = "follows"
+
+    follower_id = Column(Integer, ForeignKey("users.id"), primary_key= True)
+    following_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
+
+    follower = relationship(
+        "User", foreign_keys=[follower_id], back_populates="followers"
+    )
+
+    following = relationship(
+        "User", foreign_keys=[following_id], back_populates="following"
+    )
+
 #User Class
 class User(Base):
     __tablename__ = "users"
@@ -32,3 +47,14 @@ class User(Base):
     liked_posts = relationship(
         Post, secondary=post_likes, back_populates="liked_by_users"
     )
+
+    followers = relationship(
+        Follow, foreign_keys=[Follow.following_id], back_populates="following"
+    )
+
+    following = relationship(
+        Follow, foreign_keys=[Follow.follower_id], back_populates="follower"
+    )
+
+    followers_count = Column(Integer, default=0)
+    following_count = Column(Integer, default=0)
