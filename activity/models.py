f88d95bc8ed417e09d6c901b277b2a02dd9cd2b9
bkandh30 <kandhari.bhavya20@gmail.com>
2024-09-25 15:05:27 -0700

    activity data model

---
+++ activity/models.py
@@ -0,0 +1,20 @@
+#Data Models
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime, timezone
+
+from database import Base
+
+#Activity Class
+class Activity(Base):
+    __tablename__ = "activities"
+
+    id = Column(Integer, primary_key= True)
+    username = Column(String, nullable=False)
+    timestamp = Column(DateTime(timezone=True), nullable= False, default=lambda: datetime.now(timezone.utc))
+
+    liked_post_id = Column(Integer)
+    username_like = Column(String)
+    liked_post_id = Column(String)
+
+    follower_username = Column(String)
+    follower_user_pic = Column(String)
